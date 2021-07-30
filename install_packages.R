48a941b2fe78fab5b6656ab8192f2dcc579c61e3
mikemeredith <mike@mmeredith.net>
2021-07-30 12:28:34 +0800

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -2,16 +2,12 @@
 
 # Install or update packages needed to run the code in this repository
 # ====================================================================
-# You will need the `IPMbook` package, which is not yet on CRAN. You can install it with
-remotes::install_github("mikemeredith/IPMbook")
-
-# All the rest are available from CRAN
 
 # I suggest first running
 update.packages(ask='graphics',checkBuilt=TRUE)
 # to ensure everything is up to date, including dependencies.
 
-needed <- c("jagsUI", "scales", "MCMCglmm", "AHMbook", "wiqid", "RColorBrewer",
+needed <- c("IPMbook", "jagsUI", "scales", "MCMCglmm", "AHMbook", "wiqid", "RColorBrewer",
     "denstrip", "plotrix")
 got <- rownames(installed.packages())
 
@@ -24,6 +20,9 @@ install.packages(notgot, dependencies=TRUE)
 # If you want to try out devel versions of packages from GitHub, install these
 #  AFTER 'update.packages' as that may "downdate" to the latest CRAN version.
 # For example:
+# remotes::install_github("mikemeredith/IPMbook")
+# packageVersion("IPMbook")
+# news(package="IPMbook")
 # remotes::install_github("kenkellner/jagsUI")
 # packageVersion("jagsUI")
 # news(package="jagsUI")
